30a8e89212766f9b62003a24b751d4b76f9805be
dominiczhu <dominiczhu@tencent.com>
2021-03-05 15:40:20 +0800

    转换构造与类型转换

---
+++ computer_system/chapter3/sec4/type_transform.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+
+int main(){
+    int n=99;
+    char c=(char)n;
+
+    std::cout<<n<<std::endl;
+    std::cout<<c<<std::endl;
+    std::cout<<(int)'c'<<std::endl;
+    return 0;
+}

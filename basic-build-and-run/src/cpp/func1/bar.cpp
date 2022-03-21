528b0343b4d9ae6eb9b75a48671cd420a931d134
dominiczhu <dominiczhu@tencent.com>
2022-03-21 21:52:00 +0800

    ~

--- basic-build-and-run/src/cpp/func1/bar.cpp
+++ basic-build-and-run/src/cpp/func1/bar.cpp
@@ -1,8 +1,15 @@
-
+#include <iostream>
 // #include "../func2/foo.h"
-#include "/Users/dominiczhu/work/learn/c/vscode-cpp-demo/basic-build-and-run/src/cpp/func2/foo.h"
+// #include "/Users/dominiczhu/work/learn/c/vscode-cpp-demo/basic-build-and-run/src/cpp/func2/foo.h"
+#include <stdio.h>
+struct{
+    short a;
+    char b;
+    float c;
+} cs;
 
 int main(){
-    show();
+    // show();
+    std::cout << sizeof(cs)<< std::endl;
     return 0;
 }
